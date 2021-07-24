a1401729e497a4840bc5b67eab62eb2edbeca1bd
Dinh <nqdinhddt@gmail.com>
2021-07-24 18:58:40 +0700

    prom test

--- tests/middlewares/prom-test.js
+++ tests/middlewares/prom-test.js
@@ -1,9 +1,13 @@
 'use strict'
 
-const { normalizeStatusCode } = require('../../middlewares/prom')
+const promMiddleware = require('../../middlewares/prom')
 const expect = require('chai').expect
+const express = require('express')
+const supertest = require('supertest')
+const Prometheus = require('prom-client')
 
 describe('normalizeStatusCode', function () {
+  const normalizeStatusCode = promMiddleware.normalizeStatusCode
   it('should normalize 2xx', async () => {
     expect(normalizeStatusCode(200)).to.eqls('2xx')
     expect(normalizeStatusCode(204)).to.eqls('2xx')
@@ -17,5 +21,23 @@ describe('normalizeStatusCode', function () {
   it('should normalize 4xx', async () => {
     expect(normalizeStatusCode(500)).to.eqls('5xx')
     expect(normalizeStatusCode(501)).to.eqls('5xx')
+  })
+})
+
+describe('metricsMiddleware', () => {
+  beforeEach(() => {
+    Prometheus.register.clear()
+  })
+
+  it('should contain up 1', async () => {
+    const app = express()
+    const metricsMiddleware = promMiddleware.metricsMiddleware({})
+    app.use(metricsMiddleware)
+
+    const metrics = await Prometheus.register.metrics()
+    expect(metrics).to.contains('up 1')
+  })
+
+  it('should record a metric', async () => {
   })
 })
