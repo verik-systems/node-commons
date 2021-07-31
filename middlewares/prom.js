f2189812eee967239b1ea4115f1e934822129bcf
Dinh <nqdinhddt@gmail.com>
2021-07-31 15:06:09 +0700

    garbage collection enabled

--- middlewares/prom.js
+++ middlewares/prom.js
@@ -2,17 +2,15 @@
 
 const Prometheus = require('prom-client')
 const ResponseTime = require('response-time')
+const gcStats = require('prometheus-gc-stats')
 
 const defaultOptions = {
   prefix: '',
   customLabels: [],
   normalizeStatus: true,
   requestDurationBuckets: [0.003, 0.03, 0.1, 0.3, 1.5, 10],
-
-  requestLengthEnabled: false,
-  requestLengthBuckets: [0, 10, 100, 1000, 10000, 100000],
-  responseLengthEnabled: false,
-  responseLengthBuckets: [0, 10, 100, 1000, 10000, 100000]
+  defaultMetricEnabled: false,
+  gbMetricEnabled: false
 }
 
 /**
@@ -38,34 +36,6 @@ function requestCountGenerator (labelNames, prefix = '') {
     name: `${prefix}http_requests_total`,
     help: 'Counter for total requests received',
     labelNames
-  })
-}
-
-/**
- * @param {!Array} buckets - array of numbers, representing the buckets for
- * @param prefix - metrics name prefix
- * request length
- */
-function requestLengthGenerator (labelNames, buckets, prefix = '') {
-  return new Prometheus.Histogram({
-    name: `${prefix}http_request_length_bytes`,
-    help: 'Content-Length of HTTP request',
-    labelNames,
-    buckets
-  })
-}
-
-/**
- * @param {!Array} buckets - array of numbers, representing the buckets for
- * @param prefix - metrics name prefix
- * response length
- */
-function responseLengthGenerator (labelNames, buckets, prefix = '') {
-  return new Prometheus.Histogram({
-    name: `${prefix}http_response_length_bytes`,
-    help: 'Content-Length of HTTP response',
-    labelNames,
-    buckets
   })
 }
 
@@ -112,28 +82,25 @@ function metricsMiddleware (userOption = {}) {
     options.prefix
   )
 
-  const requestLength = options.requestLengthEnabled
-    ? requestLengthGenerator(
-        options.customLabels,
-        options.requestLengthBuckets,
-        options.prefix
-      )
-    : undefined
-
-  const responseLength = options.responseLengthEnabled
-    ? responseLengthGenerator(
-        options.customLabels,
-        options.responseLengthBuckets,
-        options.prefix
-      )
-    : undefined
-
   const up = new Prometheus.Gauge({
     name: 'up',
     help: '1 = up, 0 = not up',
     labelNames: []
   })
   up.set(1)
+
+  if (options.defaultMetricEnabled) {
+    Prometheus.collectDefaultMetrics({
+      prefix: options.prefix
+    })
+  }
+
+  if (options.gbMetricEnabled) {
+    const startGcStats = gcStats(Prometheus.register, {
+      prefix: options.prefix
+    })
+    startGcStats()
+  }
 
   /**
    * Corresponds to the R(equest rate), E(error rate), and D(uration of requests),
@@ -156,13 +123,6 @@ function metricsMiddleware (userOption = {}) {
 
     // observe normalizing to seconds
     requestDuration.observe(labels, time / 1000)
-
-    if (options.requestLengthEnabled) {
-      requestLength.observe(labels, Number(req.headers['content-length']))
-    }
-    if (options.responseLengthEnabled) {
-      responseLength.observe(labels, Number(res.headers['content-length']))
-    }
   })
 }
 
@@ -170,7 +130,5 @@ module.exports = {
   metricsMiddleware,
   normalizeStatusCode,
   requestDurationGenerator,
-  requestCountGenerator,
-  requestLengthGenerator,
-  responseLengthGenerator
+  requestCountGenerator
 }
