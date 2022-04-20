c054e495e87cf2dfd8918b4a2d9a5d23c9481ca2
huydo <huy.do@veriksystems.com>
2022-04-20 16:00:38 +0700

    fix tslint

--- libs/log.js
+++ libs/log.js
@@ -7,73 +7,73 @@ const context = require('./async-context')
 const wrapMethods = ['info', 'error', 'warn', 'debug']
 
 const logLevel = () => {
-	const index = wrapMethods.indexOf(process.env.LOG_LEVEL)
-	return index > -1 ? wrapMethods[index] : 'info'
+  const index = wrapMethods.indexOf(process.env.LOG_LEVEL)
+  return index > -1 ? wrapMethods[index] : 'info'
 }
 
 const logger = pino({
-	level: logLevel(),
-	timestamp: () => {
-		return `,"time":"${new Date().toISOString()}"`
-	}
+  level: logLevel(),
+  timestamp: () => {
+    return `,"time":"${new Date().toISOString()}"`
+  }
 })
 
 function Logger(pinoLogger) {
-	this.pinoLogger = pinoLogger
+  this.pinoLogger = pinoLogger
 }
 
 wrapMethods.forEach((item) => {
-	Logger.prototype[item] = function (msg, data, ...more) {
-		const stringifyAble =
-			data instanceof Error
-				? {
-						// Pull all enumerable properties, supporting properties on custom Errors
-						...data,
-						// Explicitly pull Error's non-enumerable properties
-						name: data.name,
-						message: data.message
-				  }
-				: data
+  Logger.prototype[item] = function (msg, data, ...more) {
+    const stringifyAble =
+      data instanceof Error
+        ? {
+            // Pull all enumerable properties, supporting properties on custom Errors
+            ...data,
+            // Explicitly pull Error's non-enumerable properties
+            name: data.name,
+            message: data.message
+          }
+        : data
 
-		this.pinoLogger[item]({
-			// message
-			msg,
-			// data
-			d: stringifyAble,
-			// more data to print
-			dEx: more.length > 0 ? more : undefined
-		})
-	}
+    this.pinoLogger[item]({
+      // message
+      msg,
+      // data
+      d: stringifyAble,
+      // more data to print
+      dEx: more.length > 0 ? more : undefined
+    })
+  }
 })
 
 module.exports.logger = function (moduleName) {
-	let bindings = {}
-	if (moduleName) {
-		bindings = { module: moduleName }
-	}
-	return new Proxy(logger, {
-		get(target, property, receiver) {
-			target = context.getStore()?.get('logger') || target
-			if (property === 'pinoLogger') {
-				target.pinoLogger[pino.symbols.chindingsSym] = asChindings(
-					target.pinoLogger,
-					bindings
-				)
-			}
-			return Reflect.get(target, property, receiver)
-		}
-	})
+  let bindings = {}
+  if (moduleName) {
+    bindings = { module: moduleName }
+  }
+  return new Proxy(logger, {
+    get(target, property, receiver) {
+      target = context.getStore()?.get('logger') || target
+      if (property === 'pinoLogger') {
+        target.pinoLogger[pino.symbols.chindingsSym] = asChindings(
+          target.pinoLogger,
+          bindings
+        )
+      }
+      return Reflect.get(target, property, receiver)
+    }
+  })
 }
 
 // Generate a unique ID for each incoming request and store a child logger in context
 // to always log the request ID
 module.exports.contextMiddleware = (req, res, next) => {
-	const child = new Logger(
-		logger.child({ requestId: req.traceId || uuid.v4() })
-	)
+  const child = new Logger(
+    logger.child({ requestId: req.traceId || uuid.v4() })
+  )
 
-	const store = new Map()
-	store.set('logger', child)
+  const store = new Map()
+  store.set('logger', child)
 
-	return context.run(store, next)
+  return context.run(store, next)
 }
--- libs/utils.js
+++ libs/utils.js
@@ -1,53 +1,54 @@
 const {
-	chindingsSym,
-	serializersSym,
-	stringifiersSym,
-	stringifySym,
-	stringifySafeSym,
-	wildcardFirstSym,
-	formattersSym
+  chindingsSym,
+  serializersSym,
+  stringifiersSym,
+  stringifySym,
+  stringifySafeSym,
+  wildcardFirstSym,
+  formattersSym
 } = require('pino/lib/symbols')
 
+
 function asChindings(instance, bindings) {
-	let value
-	let data = instance[chindingsSym]
-	const stringify = instance[stringifySym]
-	const stringifySafe = instance[stringifySafeSym]
-	const stringifiers = instance[stringifiersSym]
-	const wildcardStringifier = stringifiers[wildcardFirstSym]
-	const serializers = instance[serializersSym]
-	const formatter = instance[formattersSym].bindings
-	bindings = formatter(bindings)
-	const jsonData = JSON.parse(`{${data.slice(1)}}`)
+  let value
+  let data = instance[chindingsSym]
+  const stringify = instance[stringifySym]
+  const stringifySafe = instance[stringifySafeSym]
+  const stringifiers = instance[stringifiersSym]
+  const wildcardStringifier = stringifiers[wildcardFirstSym]
+  const serializers = instance[serializersSym]
+  const formatter = instance[formattersSym].bindings
+  bindings = formatter(bindings)
+  const jsonData = JSON.parse(`{${data.slice(1)}}`)
 
-	for (const key in bindings) {
-		if (jsonData.hasOwnProperty(key)) delete jsonData[key]
-	}
+  for (const key in bindings) {
+    if (jsonData.hasOwnProperty(key)) delete jsonData[key]
+  }
 
-	data = ',' + JSON.stringify(jsonData).replace(/[{}]/g, '')
+  data = ',' + JSON.stringify(jsonData).replace(/[{}]/g, '')
 
-	for (const key in bindings) {
-		value = bindings[key]
-		const valid =
-			key !== 'level' &&
-			key !== 'serializers' &&
-			key !== 'formatters' &&
-			key !== 'customLevels' &&
-			bindings.hasOwnProperty(key) &&
-			value !== undefined
-		if (valid === true) {
-			value = serializers[key] ? serializers[key](value) : value
-			value = (stringifiers[key] || wildcardStringifier || stringify)(
-				value,
-				stringifySafe
-			)
-			if (value === undefined) continue
-			data += ',"' + key + '":' + value
-		}
-	}
-	return data
+  for (const key in bindings) {
+    value = bindings[key]
+    const valid =
+      key !== 'level' &&
+      key !== 'serializers' &&
+      key !== 'formatters' &&
+      key !== 'customLevels' &&
+      bindings.hasOwnProperty(key) &&
+      value !== undefined
+    if (valid === true) {
+      value = serializers[key] ? serializers[key](value) : value
+      value = (stringifiers[key] || wildcardStringifier || stringify)(
+        value,
+        stringifySafe
+      )
+      if (value === undefined) continue
+      data += ',"' + key + '":' + value
+    }
+  }
+  return data
 }
 
 module.exports = {
-	asChindings
+  asChindings
 }
