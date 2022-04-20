250aa7e81432ff54cd5434c918b8092a10cfc053
huydo <huy.do@veriksystems.com>
2022-04-20 15:09:10 +0700

    add async localstorage

--- libs/log.js
+++ libs/log.js
@@ -1,66 +1,79 @@
 'use strict'
 
+const { asChindings } = require('./utils')
 const pino = require('pino')
+const context = require('./async-context')
 
 const wrapMethods = ['info', 'error', 'warn', 'debug']
 
 const logLevel = () => {
-  const index = wrapMethods.indexOf(process.env.LOG_LEVEL)
-  return index > -1 ? wrapMethods[index] : 'info'
+	const index = wrapMethods.indexOf(process.env.LOG_LEVEL)
+	return index > -1 ? wrapMethods[index] : 'info'
 }
 
 const logger = pino({
-  level: logLevel(),
-  timestamp: () => {
-    return `,"time":"${new Date().toISOString()}"`
-  }
+	level: logLevel(),
+	timestamp: () => {
+		return `,"time":"${new Date().toISOString()}"`
+	}
 })
 
-function Logger (pinoLogger) {
-  this.pinoLogger = pinoLogger
+function Logger(pinoLogger) {
+	this.pinoLogger = pinoLogger
 }
 
-wrapMethods.forEach(item => {
-  Logger.prototype[item] = function (msg, data, ...more) {
-    const stringifyAble = data instanceof Error
-      ? {
-        // Pull all enumerable properties, supporting properties on custom Errors
-          ...data,
-          // Explicitly pull Error's non-enumerable properties
-          name: data.name,
-          message: data.message
-        }
-      : data
+wrapMethods.forEach((item) => {
+	Logger.prototype[item] = function (msg, data, ...more) {
+		const stringifyAble =
+			data instanceof Error
+				? {
+						// Pull all enumerable properties, supporting properties on custom Errors
+						...data,
+						// Explicitly pull Error's non-enumerable properties
+						name: data.name,
+						message: data.message
+				  }
+				: data
 
-    this.pinoLogger[item]({
-      // message
-      msg,
-      // data
-      d: stringifyAble,
-      // more data to print
-      dEx: more.length > 0 ? more : undefined
-    })
-  }
+		this.pinoLogger[item]({
+			// message
+			msg,
+			// data
+			d: stringifyAble,
+			// more data to print
+			dEx: more.length > 0 ? more : undefined
+		})
+	}
 })
 
-const wrapRequestIdMethods = ['infoId', 'errorId', 'warnId', 'debugId']
+module.exports.logger = function (moduleName) {
+	let bindings = {}
+	if (moduleName) {
+		bindings = { module: moduleName }
+	}
+	return new Proxy(logger, {
+		get(target, property, receiver) {
+			target = context.getStore()?.get('logger') || target
+			if (property === 'pinoLogger') {
+				target.pinoLogger[pino.symbols.chindingsSym] = asChindings(
+					target.pinoLogger,
+					bindings
+				)
+			}
+			return Reflect.get(target, property, receiver)
+		}
+	})
+}
 
-wrapRequestIdMethods.forEach(item => {
-  Logger.prototype[item] = function (requestId, msg, data, ...more) {
-    const originalFn = item.replace('Id', '')
-    this.pinoLogger[originalFn]({
-      requestId: requestId,
-      msg,
-      d: data,
-      dEx: more.length > 0 ? more : undefined
-    })
-  }
-})
+// Generate a unique ID for each incoming request and store a child logger in context
+// to always log the request ID
+module.exports.contextMiddleware = (req, res, next) => {
+	const child = new Logger(
+		logger.child({ requestId: req.traceId || uuid.v4() })
+	)
 
-module.exports = function (moduleName) {
-  let bindings = {}
-  if (moduleName) {
-    bindings = { module: moduleName }
-  }
-  return new Logger(logger.child(bindings))
+	const store = new Map()
+	store.set('logger', child)
+
+	return context.run(store, next)
 }
