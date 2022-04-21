fc622be430051c7125a668d61b27129832195b1d
huydo <huy.do@veriksystems.com>
2022-04-21 09:46:00 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 module.exports = {
-	middlewares: {
-		requestId: require('./middlewares/request-id'),
-		prom: require('./middlewares/prom').metricsMiddleware,
-		context: require('./libs/log').contextMiddleware
-	},
-	libs: {
-		log: require('./libs/log').logger,
-		promEnable: require('./libs/prom-enable'),
-		promHandler: require('./libs/prom-handler')
-	}
+  middlewares: {
+    requestId: require('./middlewares/request-id'),
+    prom: require('./middlewares/prom').metricsMiddleware,
+    context: require('./libs/log').contextMiddleware
+  },
+  libs: {
+    log: require('./libs/log').logger,
+    promEnable: require('./libs/prom-enable'),
+    promHandler: require('./libs/prom-handler')
+  }
 }
